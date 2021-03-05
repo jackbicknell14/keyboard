90177d7783d7fb9c284e0ec85577038f507f9c80
Jack Bicknell <jackbicknell@gmail.com>
2021-03-05 18:16:48 +0000

    added 2nd version

--- remap.sh
+++ remap.sh
@@ -1,3 +1,3 @@
-# ~/bin/bash
+#!/bin/bash
 
-~/tkg-toolkit/mac/bin/hid_bootloader_cli -mmcu=atmega32u4 ~/sandboxes/keyboard/ergodone_ergodone_layout_ergodox_mine.hex
+~/tkg-toolkit/mac/bin/hid_bootloader_cli -mmcu=atmega32u4 ~/sandboxes/keyboard/0002/ergodone_layout_ergodox_mine.hex
